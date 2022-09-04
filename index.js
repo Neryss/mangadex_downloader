db39e6bfd81824f8095e3011b6e1c2ceb4b5b3eb
neryss <neryssistos@gmail.com>
2022-09-04 22:00:54 +0200

    documented a bit, cleaned up and added todos

--- index.js
+++ index.js
@@ -3,20 +3,29 @@ const axios = require('axios').default;
 const fs = require('fs');
 const path = require('path');
 
+/*
+// if you need to use authentification for any reason, you can use the following function
+// Just create a `.env` file with the three values M_NAME, M_MAIL, and M_PASS corresponding to your mangadex account
+*/
+
 async function	auth()
 {
 	return new Promise((resolve, reject) => {
 		axios.post("https://api.mangadex.org/auth/login", {
-		username: process.env.M_NAME,
-		email: process.env.M_MAIL,
-		password: process.env.M_PASS
-	}).then((res) => {
-		resolve(res.data)
-	}).catch((err) => {
-		reject(err);
+			username: process.env.M_NAME,
+			email: process.env.M_MAIL,
+			password: process.env.M_PASS
+		}).then((res) => {
+			resolve(res.data)
+		}).catch((err) => {
+			reject(err);
+		})
 	})
-})
 }
+
+/*
+//	return a single chapter data
+*/
 
 function	getChapter(chapterId)
 {
@@ -29,9 +38,12 @@ function	getChapter(chapterId)
 })
 }
 
+/*
+//	construct the urls needed to download the pages later on
+*/
+
 async function	construct_urls(data)
 {
-	console.log(data);
 	url = data.baseUrl + "/data/" + data.chapter.hash + "/";
 	return new Promise((resolve, reject) => {
 		tmp = data.chapter.data;
@@ -47,10 +59,11 @@ async function	construct_urls(data)
 async function	downloadFile(url, dir_name, name)
 {
 	return new Promise((resolve, reject) => {
-		console.log(url);
 		const actual_path = path.resolve(__dirname, dir_name, name);
+		if (fs.existsSync(actual_path))
+			resolve;
 		const writer = fs.createWriteStream(actual_path);
-		
+
 		axios({
 			method: "GET",
 			url: url,
@@ -65,7 +78,11 @@ async function	downloadFile(url, dir_name, name)
 	})
 }
 
-async function	getMangaChapters(id)
+/*
+// return a list containing manga volumes and other data
+*/
+
+async function	getMangaVolumes(id)
 {
 	urlParsed = "https://api.mangadex.org/manga/" + id + "/aggregate";
 	return new Promise((resolve, reject) => {
@@ -81,11 +98,14 @@ async function	getMangaChapters(id)
 	})
 }
 
+/*
+//	return chapter list from the said volume of a manga
+*/
+
 async function	getVolumeChapters(data)
 {
 	let urls = new Array(Object.keys(data.chapters).length);
 	let i = 0;
-	// console.log(data);
 	for (let key in data.chapters)
 	{
 		urls[i] = await getChapter(data.chapters[key].id);
@@ -94,6 +114,10 @@ async function	getVolumeChapters(data)
 	return (urls);
 }
 
+/*
+//	construct a list containing urls for every page of all chapters in a volume
+*/
+
 async function	construct_chapters(chapters)
 {
 	urlPack = new Array(Object.keys(chapters).length);
@@ -101,49 +125,46 @@ async function	construct_chapters(chapters)
 	for (j = 0; j < Object.keys(chapters).length; j++)
 	{
 		urlPack[i] = await construct_urls(chapters[j]);
-		urlPack[i].id = i;
+		urlPack[i].id = i + 1;
 		i++;
 	}
 	return (urlPack);
 }
 
-// TODO might need to wait for the dir creation before doing things
+/*
+//	download chapters from a volume as the following pattern:
+//	each chapter has a file name `chapter${number}`, containing the pages number from 0 to the last page as a name
+*/
+// TODO mulitple volumes at the same time
+// TODO chapters not reseting each volume
 
-async function	downloadChapters(urls, tmp)
+async function	downloadChapters(urls)
 {
 	for (i = 0; i < urls.length; i++)
 	{
+		console.log(urls[i].id);
 		if (!fs.existsSync("./chapter" + urls[i].id))
 			fs.mkdir("./chapter" + urls[i].id,{ recursive: true }, (err) => {
 				console.log("error: " + err);
 		})
 		for (j = 0; j < urls[i].length; j++)
-			await downloadFile(urls[i][j], "chapter" + i, "page" + j + ".png");
+		{
+			downloadFile(urls[i][j], "chapter" + (i + 1), "page" + j + ".png");
+			console.log(`chapter ${i + 1}, page ${j} done`);
+		}
 	}
 }
 
-// TODO transfer chapters into urls
 // TODO create flexible download solution (directories, name etc...)
-
+// TODO chapter url as argument?
 async function	main()
 {
 	// auth();
-	tmp = await getMangaChapters("259dfd8a-f06a-4825-8fa6-a2dcd7274230");
-	// console.log(tmp.volumes);
+	tmp = await getMangaVolumes("259dfd8a-f06a-4825-8fa6-a2dcd7274230");
 	lst = await getVolumeChapters(tmp.volumes[1]);
-	// console.log(tmp.volumes);
-	// console.log(lst);
-	other = await construct_chapters(lst, tmp);
+	other = await construct_chapters(lst);
 	console.log(other);
 	downloadChapters(other);
-	// let urls = new Array(Object.keys(tmp.volumes[1].chapters));
-	// let i = 0;
-	// for (let key in tmp.volumes[1].chapters)
-	// {
-	// 	urls[i] = await getChapter(tmp.volumes[1].chapters[key].id);
-	// 	i++;
-	// }
-	// console.log(urls);
 	return(0);
 }
 
